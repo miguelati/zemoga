cf441faf7820afcfcd424515a02ec8d1f0411b9c
Miguel Godoy <miguelgodoyg@gmail.com>
2022-04-15 23:36:31 -0400

    chore: Changes in Details

--- src/features/Posts/Details.tsx
+++ src/features/Posts/Details.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import {Box, Text, VStack} from 'native-base';
+import {Text, VStack} from 'native-base';
 
 const Details = () => {
   return (
-    <Box safeArea>
-      <VStack m="auto" width={350} space={3}>
-        <Text fontSize="xl" bold>
-          Details
-        </Text>
-      </VStack>
-    </Box>
+    <VStack m="auto" width={350} space={3}>
+      <Text fontSize="xl" bold>
+        Details
+      </Text>
+    </VStack>
   );
 };
 
