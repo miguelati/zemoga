d5727a12daa6e63da4447fbef7f4878de0ef958f
Miguel Godoy <miguelgodoyg@gmail.com>
2022-04-15 23:36:03 -0400

    refactor: Improve babel config

--- babel.config.js
+++ babel.config.js
@@ -1,27 +1,31 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        root: ['./'],
-        alias: {
-          /**
-           * Regular expression is used to match all files inside `./src` directory and map each `.src/folder/[..]` to `~folder/[..]` path
-           */
-          '^~(.+)': './src/\\1',
+module.exports = api => {
+  api.cache(true);
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            /**
+             * Regular expression is used to match all files inside `./src` directory and map each `.src/folder/[..]` to `~folder/[..]` path
+             */
+            '^~(.+)': './src/\\1',
+            '@native-base/icons': '@native-base/icons/lib',
+          },
+          extensions: [
+            '.ios.js',
+            '.android.js',
+            '.js',
+            '.jsx',
+            '.json',
+            '.tsx',
+            '.ts',
+            '.native.js',
+          ],
         },
-        extensions: [
-          '.ios.js',
-          '.android.js',
-          '.js',
-          '.jsx',
-          '.json',
-          '.tsx',
-          '.ts',
-          '.native.js',
-        ],
-      },
+      ],
     ],
-  ],
+  };
 };
