37a3d31066e86ac00b772f00213d51496ce2d54d
Miguel Godoy <miguelgodoyg@gmail.com>
2022-04-18 12:44:57 -0400

    feat: Add filter favs & remove all

--- src/features/Posts/Lists.tsx
+++ src/features/Posts/Lists.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useCallback} from 'react';
+import React, {useEffect, useCallback, useState} from 'react';
 import {VStack, Box} from 'native-base';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {useNavigation} from '@react-navigation/native';
@@ -9,27 +9,43 @@ import {
   ZQueryLoading,
 } from '~components';
 import i18n from '~i18n';
-import {IconNamesEnum} from '~ts/enums';
+import {IconNamesEnum, PostStateEnum} from '~ts/enums';
+import {Post} from '~ts/interfaces';
 import {usePosts} from '~api/posts/queries';
+import {storage} from '~utils';
 import {ZPostList} from './components';
 import {PostsStackParamList} from './navigation.type';
 
 const Lists = () => {
+  const [data, setData] = useState<Post[]>([]);
+  const [indexTab, setIndexTab] = useState(0);
   const {setOptions, navigate} =
     useNavigation<StackNavigationProp<PostsStackParamList>>();
 
   const onTabChange = (index: number) => {
-    console.log('onTabChange', index);
+    setIndexTab(index);
+    postsRequest.refetch();
   };
 
-  const postsRequest = usePosts();
+  const postsRequest = usePosts({
+    onSuccess: async (requestData: Post[]) => {
+      if (indexTab === 0) {
+        setData(requestData);
+      } else {
+        const favs = await storage.getState(PostStateEnum.favorite);
+        setData(requestData.filter(d => favs.includes(d.id)));
+      }
+    },
+  });
 
   const onRefreshPress = useCallback(() => {
     postsRequest.refetch();
   }, [postsRequest]);
 
-  const onDeleteAllPress = () => {
-    console.log('onDeleteAllPress');
+  const onDeleteAllPress = async () => {
+    await storage.removeAllState(PostStateEnum.favorite);
+    await storage.removeAllState(PostStateEnum.read);
+    postsRequest.refetch();
   };
 
   const onPostListItemPress = (id: string, userId: string) =>
@@ -54,10 +70,7 @@ const Lists = () => {
           onChange={onTabChange}
         />
         <ZQueryLoading flex={1} big request={postsRequest}>
-          <ZPostList
-            onPress={onPostListItemPress}
-            data={postsRequest.data || []}
-          />
+          <ZPostList onPress={onPostListItemPress} data={data || []} />
         </ZQueryLoading>
         <ZRemoveButton
           text={i18n.t('POSTS.LISTS.BUTTONS.DELETE_ALL')}
--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -7,6 +7,11 @@ export const checkState = async (state: PostStateEnum, id: string) => {
   return stateSaved.includes(id);
 };
 
+export const getState = async (state: PostStateEnum) => {
+  const res = await AsyncStorage.getItem(`@${state}`);
+  return JSON.parse(res || '[]');
+};
+
 export const saveState = async (state: PostStateEnum, id: string) => {
   const res = await AsyncStorage.getItem(`@${state}`);
   const stateSaved = JSON.parse(res || '[]');
@@ -20,4 +25,8 @@ export const removeState = async (state: PostStateEnum, id: string) => {
     `@${state}`,
     JSON.stringify([...stateSaved.filter((e: string) => e !== id)]),
   );
+};
+
+export const removeAllState = async (state: PostStateEnum) => {
+  await AsyncStorage.removeItem(`@${state}`);
 };
