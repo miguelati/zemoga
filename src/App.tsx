712efebfcf11f64080af579450fe3ef0e79eadb2
Miguel Godoy <miguelgodoyg@gmail.com>
2022-04-18 23:55:24 -0400

    feat: Add storage persistance

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import {NativeBaseProvider} from 'native-base';
 import Config from 'react-native-config';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import {NavigationContainer} from '@react-navigation/native';
 import {QueryClient, QueryClientProvider} from 'react-query';
+import {persistQueryClient} from 'react-query/persistQueryClient-experimental';
+import {createAsyncStoragePersistor} from 'react-query/createAsyncStoragePersistor-experimental';
 import {RouterStack} from '~configs/Router';
 import {configureClient} from '~api/client';
 
@@ -10,7 +13,23 @@ configureClient({
   baseURL: Config.API_URL,
 });
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      cacheTime: 1000 * 60 * 60 * 48,
+    },
+  },
+});
+
+const asyncStoragePersistor = createAsyncStoragePersistor({
+  key: '@react-query-persist',
+  storage: AsyncStorage,
+});
+
+persistQueryClient({
+  queryClient,
+  persistor: asyncStoragePersistor,
+});
 
 const App = () => {
   return (
